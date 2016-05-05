6d30b964bb1461ceba03d050b59c3a541c1a0d66
Hongyu Wang <hongywang@hotmail.com>
2016-05-05 17:54:50 -0400

    Some Changes To Repo

--- core/src/entities/GameEntity.java
+++ core/src/entities/GameEntity.java
@@ -7,11 +7,7 @@ import com.badlogic.gdx.scenes.scene2d.Event;
  *
  * Created by Hongyu Wang on 5/5/2016.
  */
-public class GameEntity extends Actor {
+public abstract class GameEntity extends Actor {
 
-    @Override
-    public boolean fire(Event event) {
-        System.out.println(event.getClass().getCanonicalName());
-        return false;
-    }
+
 }
