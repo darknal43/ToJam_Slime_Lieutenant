5fffab15ad45a083c04da814ffd9f1ba777a8693
Hongyu Wang <hongywang@hotmail.com>
2016-05-06 19:43:05 -0400

    Completed Base user information

--- core/src/driver/GameLoop.java
+++ core/src/driver/GameLoop.java
@@ -3,6 +3,8 @@ package driver;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector2;
+import state.screens.AbstractScreen;
 import state.stateManager.ScreenShell;
 
 /**
@@ -23,9 +25,12 @@ public class GameLoop extends Game {
 
     @Override
     public void render () {
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         screen.render(Gdx.graphics.getDeltaTime());
+
+
     }
 
     @Override
@@ -33,4 +38,9 @@ public class GameLoop extends Game {
 
         screen.dispose();
     }
+
+    public void updateCamera(Vector2 travelVector){
+        ((AbstractScreen)screen).updateCamera(travelVector);
+    }
+
 }
--- core/src/state/screens/GameScreen.java
+++ core/src/state/screens/GameScreen.java
@@ -1,24 +1,30 @@
 package state.screens;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import entities.GameEntity;
 import entities.Player;
-
+import tools.WorldFactory;
+import com.badlogic.gdx.graphics.Texture;
 /**
  *
  * Created by Hongyu Wang on 5/5/2016.
  */
 public class GameScreen extends AbstractScreen{
-    //TODO Remove this
 
     @Override
     protected void subclassInit() {
 
         GameEntity mainPlayer;
-
+        Texture tex;
+        Image image;
+        image = new Image(tex = new Texture("BackgroundConcepts\\granit_texture4410.jpg"));
+        stage.addActor(image);
         stage.addActor(mainPlayer = new Player("badlogic.jpg"));
         stage.setKeyboardFocus(mainPlayer);
-
-
+        image.setPosition(0, 0);
+        ((OrthographicCamera)stage.getCamera()).zoom = 1F;
+        disposables.add(tex);
     }
 
 
@@ -28,6 +34,7 @@ public class GameScreen extends AbstractScreen{
     protected void update(float delta) {
         super.update(delta);
 
+        WorldFactory.getWorld().step(delta, 8, 3);
 
 
     }
